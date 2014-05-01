e136c83e0d8f53706d94407beaf98ccb83671e6b
F Varas <tatico@gmail.com>
2014-05-01 17:53:52 +0200

    addind sudo/no sudo support

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 # http://ffmpeg.org/trac/ffmpeg/wiki/UbuntuCompilationGuide
 
+
+function wsudo {
+
 sudo apt-get remove ffmpeg x264 libav-tools libvpx-dev libx264-dev yasm
-
 sudo apt-get update
-
 sudo apt-get -y install autoconf automake build-essential checkinstall git libass-dev libfaac-dev \
   libgpac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev librtmp-dev libspeex-dev \
   libtheora-dev libtool libvorbis-dev pkg-config texi2html zlib1g-dev
@@ -53,3 +55,64 @@ sudo checkinstall --pkgname=ffmpeg --pkgversion="7:$(date +%Y%m%d%H%M)-git" --ba
 hash -r
 
 # uninstall dpkg -r ffmpeg
+
+}
+
+
+function nosudo {
+
+apt-get remove ffmpeg x264 libav-tools libvpx-dev libx264-dev yasm
+apt-get update
+apt-get -y install autoconf automake build-essential checkinstall git libass-dev libfaac-dev \
+  libgpac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev librtmp-dev libspeex-dev \
+  libtheora-dev libtool libvorbis-dev pkg-config texi2html zlib1g-dev
+
+cd
+wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
+tar xzvf yasm-1.2.0.tar.gz
+cd yasm-1.2.0
+./configure
+make
+checkinstall --pkgname=yasm --pkgversion="1.2.0" --backup=no \
+  --deldoc=yes --fstrans=no --default
+
+# uninstall dpkg -r yasm
+
+cd
+git clone --depth 1 git://git.videolan.org/x264.git
+cd x264
+./configure --enable-static
+make
+checkinstall --pkgname=x264 --pkgversion="3:$(./version.sh | \
+  awk -F'[" ]' '/POINT/{print $4"+git"$5}')" --backup=no --deldoc=yes \
+  --fstrans=no --default
+
+# uninstall dpkg -r x264
+
+cd
+git clone --depth 1 git://source.ffmpeg.org/ffmpeg
+cd ffmpeg
+./configure --enable-gpl --enable-libass --enable-libfaac --enable-libmp3lame \
+  --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libspeex \
+  --enable-librtmp --enable-libtheora --enable-libvorbis --enable-libx264 \
+  --enable-nonfree --enable-version3
+
+make
+checkinstall --pkgname=ffmpeg --pkgversion="7:$(date +%Y%m%d%H%M)-git" --backup=no \
+  --deldoc=yes --fstrans=no --default
+hash -r
+
+# uninstall dpkg -r ffmpeg
+
+}
+
+SUDO=$1
+
+if [ x$SUDO = x"sudo" ]
+	#then wsudo
+	then echo "wsudo"
+elif which sudo 
+	then echo "wich sudo"
+else 
+	echo "nosudo"
+fi

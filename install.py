4daf59bd0ea1e11d6b37cc822a46b9e65e198eb2
F Varas <tatico@gmail.com>
2014-09-07 11:32:14 +0200

    fixing issue #12

---
+++ install.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+'''This is the installer script.
+It can download a static 32/64bit linux binary or build ffmpeg from source'''
+
+### TODO
+
+# uname
+
+# download
+
+# create ~/flimeo/local/bin
+
+# install ffmpeg (built or downloaded)
+
+# create .config file
+#example
+# lets create that config file for next time...
+#cfgfile = open("c:\\next.ini",'w')
+# add the settings to the structure of the file, and lets write it out...
+#Config.add_section('Person')
+#Config.set('Person','HasEyes',True)
+#Config.set('Person','Age', 50)
+#Config.write(cfgfile)
+#cfgfile.close()
+
+#read file
+#import ConfigParser
+#Config = ConfigParser.ConfigParser()
+#Config.read("c:\\tomorrow.ini")
+#Config.sections()
--- install.sh
+++ install.sh
@@ -7,12 +7,28 @@
 mkdir -p tmp
 
 #
+# Check arch
+#
+
+ARCH=`uname -m`
+
+#
 # Install ffmpeg
 #
 
 # Static Binary
 # BINARY_URL=http://0.0.0.0/ffmpeg.tar.gz
-BINARY_URL=http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.latest.tar.gz
+
+####
+###  FIX THIS WITH UNAME
+####
+
+if [ "$ARCH" = "i686" ]
+  then BINARY_URL=http://ffmpeg.gusari.org/static/32bit/ffmpeg.static.32bit.latest.tar.gz
+elif [ "$ARCH" = 'x86_64']
+  then BINARY_URL=http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.latest.tar.gz
+fi
+
 wget $BINARY_URL --output-document=tmp/ffmpeg.tar.gz
 tar -C tmp -xf tmp/ffmpeg.tar.gz
 mkdir -p local/bin

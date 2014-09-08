cb6448a0904bd16ce64ea8feeeb2f4af3f861e3c
Alejandro Varas <alej0varas@gmail.com>
2014-09-08 12:08:48 -0300

    Added processor arch detection
    - Install script download ffmpeg binary depending on processor arch

--- install.sh
+++ install.sh
@@ -11,8 +11,9 @@ mkdir -p tmp
 #
 
 # Static Binary
-# BINARY_URL=http://0.0.0.0/ffmpeg.tar.gz
-BINARY_URL=http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.latest.tar.gz
+ARCH=`getconf LONG_BIT`
+# BINARY_URL=http://0.0.0.0:8000/ffmpeg-"$ARCH"bit.tar.gz
+BINARY_URL=http://ffmpeg.gusari.org/static/"$ARCH"bit/ffmpeg.static."$ARCH"bit.latest.tar.gz
 wget $BINARY_URL --output-document=tmp/ffmpeg.tar.gz
 tar -C tmp -xf tmp/ffmpeg.tar.gz
 mkdir -p local/bin
